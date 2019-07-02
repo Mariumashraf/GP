a392c5b7d69b6e3799a716d04dca02ddde8a9a2b
Mariumashraf <mariamashraf910@gmail.com>
2019-07-02 13:25:45 +0200

    Hearingapp

---
+++ NEW hearingapp/HearingApplication/app/src/main/java/rehabilitationtool/rehabilitationtoolgp/com/hearingapplication/ExportComplete.java
@@ -0,0 +1,33 @@
+package rehabilitationtool.rehabilitationtoolgp.com.hearingapplication;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+
+public class ExportComplete extends AppCompatActivity {
+
+    public void gotoMain(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export_complete);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+
+
+
+
+}
+
---
+++ NEW hearingapp/HearingApplication/app/src/main/java/rehabilitationtool/rehabilitationtoolgp/com/hearingapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package rehabilitationtool.rehabilitationtoolgp.com.hearingapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button StartTest = (Button) findViewById(R.id.startTest);
+        ImageView live = (ImageView) findViewById(R.id.live);
+
+        StartTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this,StartTest.class);
+                startActivity(intent1);
+
+            }
+        });
+
+        live.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this,MainEqualizer.class);
+                startActivity(intent1);
+
+            }
+        });
+    }
+}
